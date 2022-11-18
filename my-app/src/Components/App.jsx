dedea55f1cd29299cf29375fec7ee80243b61fa8
Zbigniew Kulczński <z-kulczynski@o2.pl>
2022-11-18 11:51:23 +0100

    code and style added

--- my-app/src/Components/App.jsx
+++ my-app/src/Components/App.jsx
@@ -1,24 +1,29 @@
 import React, { Component } from 'react';
 import Countdown from './Countdown';
+import EditEvent from './EditEvent';
+import '../App.css';
 
 class App extends Component {
 	constructor() {
 		super();
 		this.state = {
 			events: [
-				{ id: 0, name: "breakfast", time: "07:00" },
-				{ id: 1, name: "lunch", time: "15:00" },
-				{ id: 2, name: "dinner", time: "19:00" }
-			]
-		}
+				{ id: 0, name: 'breakfast', time: '07:00' },
+				{ id: 1, name: 'lunch', time: '15:00' },
+				{ id: 2, name: 'dinner', time: '19:00' },
+			],
+		};
 	}
 
 	render() {
-		const events = this.state.events.map(el => {
-			return <Countdown name={el.name} time={el.time} />
-		})
+		const events = this.state.events.map((el) => {
+			return <Countdown key={el.id} name={el.name} time={el.time} />;
+		});
 		return (
-			<div>{events}</div>
+			<div className="app">
+				{events}
+				<EditEvent />
+			</div>
 		);
 	}
 }
